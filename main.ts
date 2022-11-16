beb981f2725de21896c1ac92cdf42ef889f06e77
Roman Kubiv <romankubiv101@gmail.com>
2022-11-16 01:41:21 +0000

    Add plugin settings
    - Root folder path
        - with input validation
    - User interface mode dropdown

--- main.ts
+++ main.ts
@@ -1,32 +1,22 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, TFolder, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SettingsPNO {
+	projectFolderPath: string;
+	pickerMode: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SettingsPNO = {
+	projectFolderPath: '/',
+	pickerMode: "flat",
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: SettingsPNO;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -36,15 +26,7 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -66,16 +48,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new PNOSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -98,7 +71,7 @@ class SampleModal extends Modal {
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Woah! My plugin!');
 	}
 
 	onClose() {
@@ -107,7 +80,7 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class PNOSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -120,18 +93,39 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Project Note Opener.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Root Project Folder Path')
+			.setDesc('The path to the folder which contains your projects')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Please enter a path')
+				.setValue(this.plugin.settings.projectFolderPath)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					let folder = this.app.vault.getAbstractFileByPath(value);
+					if ( folder && folder instanceof TFolder ){
+						console.log('New Project Folder Path: ' + value);
+						this.plugin.settings.projectFolderPath = value;
+						text.inputEl.removeClasses(["opn_error"])
+						new Notice("Saved Project Folder Path")
+					} else {
+						text.inputEl.addClasses(["opn_error"])
+					}
+					
 					await this.plugin.saveSettings();
-				}));
+				}))
+
+		new Setting(containerEl)
+		.setName("Project picker mode")
+		.setDesc('Flat: Display all project notes in the root project folder as \'pathToProjectFolder/projectFolderName\'Folder: Chose a top level folder in the root project folder and then between any subfolders (if necessary)')
+		.addDropdown(dropdown => dropdown
+			.addOption("flat","flat")
+			.addOption("folder","folder")
+			.setValue(this.plugin.settings.pickerMode)
+			.onChange(async (value) => {
+				console.log("New picker mode", value);
+				this.plugin.settings.pickerMode = value;
+			})
+		);
 	}
 }
